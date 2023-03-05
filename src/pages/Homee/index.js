6c9da7c4f5272bd62cdd6156c9103d7098319f21
unknown <ss5815190@gmail.com>
2023-03-05 21:13:32 +0800

    createContext

--- src/pages/Homee/index.js
+++ src/pages/Homee/index.js
@@ -1,10 +1,12 @@
 import Commodity from'./cpmponents/commodity.js';
-import {useState,useEffect}from'react';
+import {useState,useEffect,createContext}from'react';
 const Homee=()=>{
+	const MyContext = createContext();
 	const [data, setData] = useState([]);
   	const [isLoading, setIsLoading] = useState(true);
   	const [cart, setCart] = useState([]);//購物車
   	const [totalPrice, setTotalPrice] = useState(0);
+
   	// 加入商品
   	const AddToCart = (product) => {
     setCart([...cart, product]);
@@ -26,9 +28,11 @@ const Homee=()=>{
     return <div className="loading">Loading...</div>;
   }
 	return(
-		<div className="homee">
-			<Commodity data={data} AddToCart={AddToCart} cart={cart}/>
-		</div>
+		<MyContext.Provider value={{ data,setData,cart,setCart,totalPrice,setTotalPrice}}>
+			<div className="homee">
+				<Commodity data={data} AddToCart={AddToCart} cart={cart}/>
+			</div>
+		 </MyContext.Provider>
 		)
 }
 export default Homee;
